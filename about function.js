75283f3a61a917f87a636074becb3fb74ebe7bb6
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-02-21 10:16:33 +0530

    function

---
+++ about function.js
@@ -0,0 +1,70 @@
+
+// function stored inside variable
+let x=function(){
+console.log("some functions are stored in variable");
+};
+
+x();
+
+
+
+//function using return 
+
+function sum(a,b){
+    return a+b;
+}
+let z=sum(3,5);
+console.log(z)
+
+
+//function using console
+  
+
+function cons(v,b){
+    console.log(v*b);
+}
+
+cons(8,9);
+
+
+function eatbreakfast(item){
+    console.log("i will eat my breakfast before",item)
+}
+eatbreakfast("idly");
+
+
+//parameter and function
+
+function eatbreakfasts(item,time){
+    console.log("i will eat "+item+" as my breakfast at",+time)
+
+}
+eatbreakfasts("breadjam",10)
+
+
+//when we pass function as a argument
+
+function breakfast(item,time,dobrush){
+    dobrush()
+    console.log("i will eat "+item+" as my breakfast at ",+time)
+}; 
+breakfast("bunmakhan",12,dobrush);
+
+function dobrush(){
+    console.log("do brush on time");
+ 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
