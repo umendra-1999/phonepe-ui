c58309c9b98882e87c6c5d7818ef5abab07664a5
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-02-23 10:26:21 +0530

    slide

---
+++ slideshow/album.js
@@ -0,0 +1,6 @@
+function storeimage(){
+    let url=document.getElementById('url').value;
+    let images=JSON.parse(localStorage.getItem('images'))||[];
+    images.push(url);
+    localStorage.setItem('images',JSON.stringify(images));
+};
