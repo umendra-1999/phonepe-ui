530b9f5803f51e3143fcdf4f7122e1b1f0dd0fc2
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-04 20:13:14 +0530

    practice

---
+++ practice code/break.js
@@ -0,0 +1,21 @@
+
+//using break statement
+let j="";
+for (let i=0;i<=10;i++){
+    if(i==9){
+    break;
+}
+j=j+i+"";
+}
+console.log(j)
+
+
+
+// using continue  statement
+for(let k=0;k<=10;k++){
+    if(k==5){
+        continue;
+
+    }
+    console.log(k)
+}
