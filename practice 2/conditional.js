cd3695fde30c6343287521b7f31f40af4078d888
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-05 10:44:40 +0530

    3

---
+++ practice 2/conditional.js
@@ -0,0 +1,11 @@
+let a=parseInt(prompt("enter the value of a"));
+let b=parseInt(prompt("enter the value of b"));
+
+c=a>b;
+
+if(c){
+    console.log("a is bigger")
+}
+else{
+    console.log("b is biiger")
+}
