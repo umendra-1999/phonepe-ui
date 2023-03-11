378a73def9893ae3740a77708c144d48f53d3a94
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-11 14:45:25 +0530

    6

---
+++ practice 6/function.js
@@ -0,0 +1,48 @@
+function multi()
+{
+    console.log(7*5)
+}
+multi()
+multi()
+multi()
+multi()//mtlb jitni baar function ko call karengee utni baar output ayega 
+
+function add(){
+    let a=19;
+    let b=20;
+    console.log(a+b);
+}
+function multiply(){
+    let x=34;
+    let y=2;
+    console.log(x*y)
+}
+function sub(){
+    let m=32;
+    let n=65;
+    console.log(m-n)
+}
+function div(){
+    let p=45;
+    let q=9;
+    console.log(p/q);
+}
+console.log("function next answere is here")
+add()
+multiply()
+sub()
+div()
+
+
+
+
+
+//using parameterized function
+
+function superman(o,l){
+    console.log(o+l)
+}
+console.log("parameterized function answere is here")
+superman(23,4)
+superman(34,67)
+superman(1897239,345)
