c4efb87b884c23c2e065a5e4ea644f4bdeec337f
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-12 08:24:25 +0530

    7

---
+++ practice 7/functions.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    console.log(a+b)
+}
+function sub(m,n){
+    console.log(m-n)
+}
+function multi(p,q){
+    console.log(p*q)
+}
+function divide(x,y){
+    console.log(x/y)
+}
+add(23,45)
+sub(98,65)
+multi(23,12)
+divide(245,15)
