f9a971e99868b9e4ad9ab354f4250c0dc2ab49a8
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-19 10:29:13 +0530

    14

---
+++ practice 14/loops.js
@@ -0,0 +1,28 @@
+
+//using while loop
+
+let n=Number.parseInt(prompt("enter the number"))
+
+let i=1;
+while(i<n)
+{
+    console.log(i)
+    i++;
+}
+
+//using do while loop
+
+let d=7;
+let k=0;
+do{
+    console.log(k)
+    k++;
+}
+while(k<d)
+
+
+//using for loop
+
+for(let o=0;o<5;o++){
+    console.log("hello world")
+}
