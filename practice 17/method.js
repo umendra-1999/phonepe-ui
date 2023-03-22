ad889225a71db4ffe8a7733514e4b048b2466a59
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-22 13:44:33 +0530

    17

---
+++ practice 17/method.js
@@ -0,0 +1,7 @@
+let obj={
+    details:function(){
+        console.log("rocky");
+        console.log("34")
+    }
+}
+obj["details"]()
---
+++ practice 17/multiarray.js
@@ -0,0 +1,9 @@
+function multi(arr1,arr2){
+    let newrr=[];
+    for (let i=0;i<=arr1.length-1;i++){
+        let product=arr1[i]*arr2[i];
+        newrr.push(product)
+    }
+    console.log(newrr)
+}
+multi([12,34,6,7,2],[2,2,3,8,9])
