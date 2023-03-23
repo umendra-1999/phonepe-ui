32e80b89872524cb2e29aab1b87775aceecd5c2a
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-23 10:05:30 +0530

    18

---
+++ practice 18/nestedobj.js
@@ -0,0 +1,16 @@
+let details={
+    name:"umendra",
+    age:24,
+    address:{
+        city:"lucknow",
+        street:"456",
+        house:"100",
+        pin:"475912"
+    }
+};
+console.log(details.address)//is syntax ke through ham address par pahuch jayenge
+console.log(details.address.street)//is syntax ke through ham address ke baad ham street ko access kar skte h 
+
+for(let key in details){   //in object we can use only for in loop because there is no concept of index number
+console.log(key,details[key])
+}
