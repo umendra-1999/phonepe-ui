74ebb957059ed08dce6c57ebc2579a6b57a5ca95
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-24 15:04:45 +0530

    19

---
+++ practice 19/obj111.js
@@ -0,0 +1,18 @@
+let arr=[7,7,6,5,5,4,34,4,4,21,34,32];
+let obj={};
+for(let i=0;i<arr.length-1;i++){
+    let one=arr[i];
+    if(obj[one]==undefined){
+        obj[one]=1;
+    }
+    else{
+        obj[one]++;
+    }
+}
+let sum=0;
+for(key in obj){
+    if(obj[key]==1){
+        sum=sum+Number.parseInt(key);
+    }
+}
+console.log(sum)
