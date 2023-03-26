d9e85d0a29e045af27a291c0fef5dfdca51d6fae
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-26 14:43:01 +0530

    21

---
+++ practice 21/objects11.js
@@ -0,0 +1,12 @@
+let empdata=[
+    {name:"aman",age:45,location:"delhi"},
+    {name:"rahul",age:34,location:"mumbai"},
+    {name:"ajay",age:39,location:"delhi"},
+    {name:"roman",age:48,location:"mumbai"}
+];
+for(let i=0;i<=empdata.length-1;i++){
+    let obj=empdata[i];
+    if(obj["location"]=="delhi"){
+        console.log(obj["name"]);
+    }
+}
