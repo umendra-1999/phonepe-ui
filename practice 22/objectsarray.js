d7ee29fd8ada5b1fb4f3cff98105559572aad7af
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-27 12:21:11 +0530

    22

---
+++ practice 22/objectsarray.js
@@ -0,0 +1,33 @@
+//from the given arrays make an array of object
+
+let products=["mackbook","iphone","ipad","airpods"];
+let price=[45000,75000,53466,6754];
+let arr=[];
+for(let i=0;i<products.length;i++){
+    let obj={};
+    obj["name"]=products[i];
+    obj["price"]=price[i];
+    arr.push(obj);
+}
+console.log(arr)
+
+
+
+
+
+//find average rating of products
+
+let amazon=[
+    {name:"iphone",price:70000,rating:4.5},
+    {name:"mackbook",price:50000,rating:4},
+    {name:"airpods",price:6543,rating:3.6},
+    {name:"xbox",price:10000,rating:5}
+]
+let sum=0;
+for(let j=0;j<=amazon.length-1;j++){
+    let product=amazon[j];
+    sum=sum+product["rating"]
+}
+console.log("sum is ",sum)
+console.log("average is",sum/amazon.length)
+
