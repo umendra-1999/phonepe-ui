f74d0a44dfbc50045c327687e324ce0a0482da13
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-03-30 10:43:12 +0530

    25

---
+++ practice 25/return.js
@@ -0,0 +1,13 @@
+function add(a,b){
+    return(a+b)
+}
+function sub(m){
+    return(m*5)
+}
+function main(x){
+    return(x**2)
+}
+let step1=add(10,20);
+let step2=sub(step1);
+let step3=main(step2)
+console.log(step3)
