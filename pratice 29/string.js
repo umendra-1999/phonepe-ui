e09e4f2e4a1a5e35064fc39410b95a0b6824f4a9
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-04-03 07:59:09 +0530

    29

---
+++ pratice 29/string.js
@@ -0,0 +1,40 @@
+//to add something in a string
+
+// let str="superman";
+// str=str+" , batman";
+// console.log(str)
+
+//something update and replace in the string
+
+// let st="donald";
+// let arr=[];
+// for(let i=0;i<=st.length-1;i++){
+// arr.push(st[i])
+// }
+// console.log(arr)
+
+// arr[0]="r";
+// console.log(arr)
+// let bag="";
+// for (let j=0;j<=arr.length-1;j++){
+
+
+// }
+// console.log(bag)
+
+
+//another way to do thus coding
+
+let strr="donald";
+let new_strr="";
+
+for(let i=0;i<=strr.length-1;i++){
+    if(i==0){
+        new_strr=new_strr+"r";
+    }
+    else{
+        new_strr=new_strr+strr[i];
+
+    }
+}
+console.log(new_strr)
