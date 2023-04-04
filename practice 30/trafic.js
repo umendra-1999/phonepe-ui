1ebb5812692fc332e4dcd0fc4b6ed2a843f6c7fa
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-04-04 16:20:28 +0530

    30

---
+++ practice 30/trafic.js
@@ -0,0 +1,29 @@
+let light=prompt("enter the color of trafic light")
+
+if(light=="red"){
+    console.log("stop")
+}
+else if(light=="yellow"){
+    console.log("wait")
+}
+else if(light=="green"){
+    console.log("go")
+}
+else{
+    alert("the trafic light is not working properly")
+}
+
+
+//another example of discount on price rate 
+
+let price=Number.parseInt(prompt("enter the price"))
+
+if(price<="799"){
+    console.log("discount of 20%")
+}
+ else if(price<="999"||price>="1200"){
+    console.log("discount of 40%")
+ }
+ else if(price>=50){
+    console.log("no discount")
+ }
