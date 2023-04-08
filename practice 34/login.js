63310b96be3b61590fed9dba89a8f738fa685c0e
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2023-04-08 09:17:21 +0530

    34

---
+++ practice 34/login.js
@@ -0,0 +1,11 @@
+function functionlogin(){
+    let log=document.getElementById("btn")
+    alert("login successful")
+}
+
+
+
+function funcotp(){
+    let pin=document.getElementById("otp")
+    alert("OTP send to your mobile no.")
+}
